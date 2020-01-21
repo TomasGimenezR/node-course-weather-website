930dce96644ac94495106e3a8ee4b61d834be9da
TomasGR <tomasgimenezr@gmail.com>
2020-01-21 13:07:23 -0300

    Added new information to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ request({ url, json: true }, (error, {body}) => {
     }else if (body.error) {
         callback('Unable to find location. Try another search')
     }else{
-        callback(undefined,body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degrees Celcius. There is a ' + body.currently.precipProbability + '% rain chance.')
+        callback(undefined,body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degrees Celcius. \r\nThere is a ' + body.currently.precipProbability + '% rain chance. \r\nThe apparent temperature is ' + body.currently.apparentTemperature + ' degrees Celcius, with a temperature high of '+ body.daily.data[0].temperatureHigh + ' degrees Celcius and a low of ' + body.daily.data[0].temperatureLow + ' degrees Celcius. \r\n Tomorrow will be ' + body.daily.data[1].summary )
     }
 })
 }
